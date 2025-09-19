450e412674f41e70065a5a75674999cb83e44d27
kartik-jain-100398 <kj100398@gmail.com>
2025-09-19 03:27:35 +0100

    Setting up structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Actuarial Pricing Library
+
+This project contains actuarial pricing models and simulations.
---
+++ actuarial_pricing/__init__.py
@@ -0,0 +1,2 @@
+# pip install numpy pandas scipy matplotlib seaborn scikit-learn statsmodels openpyxl
+# pip install pytest black ruff mypy
